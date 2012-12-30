4302525f97c6bc70f85e4c925842d2f21c31fa47
Vyacheslav Vorobyov <vjache@gmail.com>
2012-12-31 01:45:39 +0400

    Code refactored.

--- src/confetti_rest_api_handler.erl
+++ src/confetti_rest_api_handler.erl
@@ -26,19 +26,17 @@
 %%
 
 %%
-%% Exported Functions
-%%
--export([]).
-
-%%
 %% API Functions
 %%
 
--export([init/3]).
--export([content_types_provided/2]).
--export([hello_to_html/2]).
--export([hello_to_json/2]).
--export([hello_to_text/2]).
+-export([init/3,
+		 content_types_provided/2,
+		 to_html/2, 
+		 to_json/2, 
+		 to_text/2]).
+
+-record(args, {subjects, subject, variables, prettify}).
+-define(UNDEF, undefined).
 
 init(_Transport, _Req, []) ->
 	io:format("INIT:~p~n", [_Req]),
@@ -46,12 +44,12 @@ init(_Transport, _Req, []) ->
 
 content_types_provided(Req, State) ->
 	{[
-%% 		{<<"text/html">>, hello_to_html},
-		{<<"application/json">>, hello_to_json},
-		{<<"text/plain">>, hello_to_text}
+%% 		{<<"text/html">>, to_html},
+		{<<"application/json">>, to_json},
+		{<<"text/plain">>, to_text}
 	], Req, State}.
 
-hello_to_html(Req, State) ->
+to_html(Req, State) ->
 	Body = <<"<html>
 <head>
 	<meta charset=\"utf-8\">
@@ -68,55 +66,76 @@ hello_to_html(Req, State) ->
 %	* /ctx1/ctx2/.../ctxN?subject=SOME_SUBJECT&?variables=COMMA_SEPARATED_LIST_OF_VARs
 %	* /ctx1/ctx2/.../ctxN?subjects=COMMA_SEPARATED_LIST_OF_SOME_SUBJECTs
 % and also flag "&prettify=true" can be add to any above.
-hello_to_json(Req, State) ->
+to_json(Req, State) ->
+	try
 	ContextPath = [_|_] = req(path_info, Req),
-	case req_qs_val(<<"prettify">>, Req) of
-		<<"true">> 	-> Opts = [prettify, json];
-		_ 			-> Opts = [json]
-	end,
-	Subjects 	= req_qs_val(<<"subjects">>, Req),
-	JSON = case Subjects of
-			   undefined ->
-				   Subject 	= req_qs_val(<<"subject">>, Req),
-				   Variables 	= req_qs_val(<<"variables">>, Req),
-				   case {Subject, Variables} of
-					   {undefined, _ } -> % No subject, hence raise error
-						   cowboy_req:reply(404,Req),
-						   exit(subject_expected);
-					   {_, undefined} -> % Subject only, hence all variables of a subject requested
-						   confetti_backend:get_subject_config(get_backend(Req), ContextPath, Subject, Opts);
-					   {_,_} -> % Subject an variables are defined, hence a subset of variables of a subject requested
-						   case binary:split(Variables, <<",">>, [global]) of
-							   [Variable] ->
-								   confetti_backend:get_subject_variable_config(
-									 get_backend(Req), ContextPath, Subject, Variable, Opts);
-							   VariableList ->
-								   confetti_backend:get_subject_variables_config(
-									 get_backend(Req), ContextPath, Subject, VariableList, Opts)
-						   end
-				   end;
-			   _ ->
-				   SubjectList = binary:split(Subjects, <<",">>, [global]),
-				   confetti_backend:get_subjects_config(
-					 get_backend(Req), ContextPath, SubjectList, Opts)
-		   end,
-	{JSON, Req, State}.
+	Args = parse_args(Req),
+	Opts = opts(Args, [json]),
+	JSON = to_json(Req, ContextPath, Args, Opts, State),
+	{JSON, Req, State}
+	catch
+		_:bad_arg_comb ->
+			cowboy_req:reply(404,Req);
+		_:{unexpected_arguments, _ListUnexpected} ->
+			cowboy_req:reply(404,Req)
+	end.
 
-hello_to_text(Req, State) ->
-	hello_to_json(Req, State).
+to_json(_Req, _ContextPath, #args{subject = Subject, variables = Variables, subjects = Subjects}, _Opts, _State)
+  when Subjects =/= ?UNDEF andalso (Subject =/= ?UNDEF orelse Variables =/= ?UNDEF) ->
+	exit(bad_arg_comb);
+to_json(Req, ContextPath, #args{subject = Subject, variables = ?UNDEF, subjects = ?UNDEF}, Opts, _State) ->
+	confetti_backend:get_subject_config(get_backend(Req), ContextPath, Subject, Opts);
+to_json(Req, ContextPath, #args{subject = Subject, variables = Variables, subjects = ?UNDEF}, Opts, _State) ->
+	case split(Variables) of
+		[Variable] ->
+			confetti_backend:get_subject_variable_config(
+			  get_backend(Req), ContextPath, Subject, Variable, Opts);
+		VariableList ->
+			confetti_backend:get_subject_variables_config(
+			  get_backend(Req), ContextPath, Subject, VariableList, Opts)
+	end;
+to_json(Req, ContextPath, #args{subject = ?UNDEF, variables = ?UNDEF, subjects = Subjects}, Opts, _State) ->
+	SubjectList = split(Subjects),
+	confetti_backend:get_subjects_config(
+	  get_backend(Req), ContextPath, SubjectList, Opts).
+
+to_text(Req, State) ->
+	to_json(Req, State).
 
 %%
 %% Local Functions
 %%
 
+parse_args(Req) ->
+	{List,_}=cowboy_req:qs_vals(Req),
+	case lists:foldl(
+		   fun(K, {Args, L, N})->
+				   KB = atom_to_binary(K, latin1),
+				   case lists:keytake(KB, 1, L) of
+					   {value, {_, Value}, L1} ->
+						   {setelement(N, Args, Value), L1, N + 1};
+					   false ->
+						   {Args, L, N + 1}
+				   end
+		   end, {#args{}, List, 2}, record_info(fields, args)) of
+		{Args, [], _} ->
+			Args;
+		{_Args, ListUnexpected, _} ->
+			exit({unexpected_arguments, ListUnexpected})
+	end.
+
 req(Func,Req) ->
 	{V,_}=cowboy_req:Func(Req),
 	V.
 
-req_qs_val(Name, Req) ->
-	{V,_}=cowboy_req:qs_val(Name, Req),
-	V.
-
 get_backend(_Req) ->
 	default_backend.
+
+split(Binary) ->
+	binary:split(Binary, <<",">>, [global]).
+
+opts(#args{prettify = Prettify}, Opts0) when Prettify orelse Prettify == <<"true">> ->
+	[ prettify | Opts0];
+opts(_Args, Opts0) ->
+	Opts0.
 
